d4ca3e17c428c3dcb5911dfa1e968d7553e29851
unknown <aswinhss123@gmail.com>
2024-09-20 11:40:23 +0530

    vercel

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Header from './components/Header.vue'
+
+</script>
+
+<template>
+  <div>
+    <Header></Header>
+    <main >
+      <div class=" mx-auto max-w-full   md:max-w-7xl lg:max-w-full  sm:py-6  sm:px-6 lg:px-8 lg:py-0 ">
+        <router-view >
+      
+        </router-view>
+        
+      </div>
+    </main>
+  </div>
+</template>
+
+
+<style >
+</style>
